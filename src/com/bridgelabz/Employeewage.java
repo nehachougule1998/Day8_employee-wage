271add0e918ba62ad2ca8857dad6536305575d55
nehachougule1998 <nchougule734@gmail.com>
2022-03-18 13:49:41 +0530

    UC9 added

--- src/com/bridgelabz/Employeewage.java
+++ src/com/bridgelabz/Employeewage.java
@@ -2,52 +2,86 @@ package com.bridgelabz;
 import java.util.Random;
 import java.util.Scanner;
 public class Employeewage{
-	 static final int full_time = 1;
-	 static final int part_time = 2;
-	 public static void employeeAttendance(String name, int wagePerHrs, int workingDay, int maxworkingHrs){
-	
-		     int wage_per_hrs = 20;
-	         int full_day_hrs = 8;
-	         int part_time_hrs = 4;
-	         int working_days =20;
-	         int montlyl_Wage = 0;
-	         int wage;
-	         int montly_Wage=0;
-	         int hours=0;
-	         int days=1;
-	         
-	        
-	         System.out.println("Company name : "+ name);
-	         int check=(int)Math.floor(Math.random() *10) % 3;
-	         switch (check) {
-	         case full_time : 
-	             System.out.println("Employee is full time present"); 
-	             wage = full_day_hrs * wage_per_hrs;
-	             System.out.println("emp wage : " +wage);
-	             montly_Wage  = montly_Wage + wage;
-	             hours = hours + full_day_hrs;
-	        	 break;
-	        
-	         case part_time : 
-	             System.out.println("Employee is part time present"); 
-	             wage = part_time_hrs * wage_per_hrs;
-	             System.out.println("emp wage : " +wage);
-	             montly_Wage  = montly_Wage + wage;
-	             hours = hours + part_time_hrs;
-	        	 break;
-		                 
-	         default:
-	        	 System.out.println("employee is absent");
-	          }
-	         System.out.println(" total wage for a Month : " + montly_Wage);
-	         System.out.println("Total working Hours  : "+hours);
-	         System.out.println(" total working days  : "+ days);
-	         }
-	         public static void main(String[] args) {
-	    		System.out.println("Welcome to Employee Wage Computation");
-	    		Employeewage.employeeAttendance("google", 23, 56, 78);
-	    		Employeewage.employeeAttendance("microsoft", 39, 28, 64);
-	    	}
+	static final int full_time= 8;
+    static final int part_time= 4;
+    int wagePerHrs;
+    int workingDay;
+    int maxworkingHrs;
+    String name;
+    int monthlyWage;
+    
+    void companyMonthlyWage(int monthlyWage) {
+ 	   this.monthlyWage=monthlyWage;
+    }
+    
+    public Employeewage(String name, int wagePerHrs, int workingDay, int maxworkingHrs, int monthlyWage) {
+		super();
+		this.wagePerHrs = wagePerHrs;
+		this.workingDay = workingDay;
+		this.maxworkingHrs = maxworkingHrs;
+		this.name = name;
+		this.monthlyWage = monthlyWage;
+	}
 
-	  }
-	    
+	public void employeeAttendance() {
+		 int empHrs = 0;
+         int monthlyWage = 0;
+         int days = 1;
+         int hours = 0;
+         System.out.println("Company name : "+ name);
+         while(days < workingDay && hours < maxworkingHrs) {
+         	int empCheck = (int)Math.floor(Math.random() *10) % 3;
+         	int remaining_Hours=100-hours;
+         	if(remaining_Hours%8<1) {
+         		System.out.println(" Employee is present for part time");
+         		int partialDailyWage = part_time * wagePerHrs;
+         		System.out.println("Daily Wage :Rs."+partialDailyWage);
+         		monthlyWage=monthlyWage+partialDailyWage;
+         		hours=hours+part_time;
+         }
+         	days ++;
+         	
+        switch (empCheck) {
+        case 1 :
+               System.out.println("Employee is Full time presnt");
+               int dailyWage = full_time * wagePerHrs;
+               System.out.println("Daily wage : "+dailyWage);
+               monthlyWage = monthlyWage + dailyWage;
+               hours = hours + full_time;
+               break;
+        case 2 :
+               System.out.println("Employee is Part time presnt");
+               int dailyWage_par = part_time * wagePerHrs;
+               System.out.println("Daily wage : "+ dailyWage_par);
+               monthlyWage = monthlyWage + dailyWage_par;
+               hours = hours+part_time;
+               break;
+        default :
+                System.out.println("Employee absent");
+        }  
+     } 
+         
+               System.out.println("Monthly Wage : " + monthlyWage);
+               System.out.println("Total Hours  : "+hours);
+               System.out.println("day  : "+ days);
+	}
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+		
+		Employeewage employee1 = new Employeewage("google", 23, 45, 56, 78);
+		employee1.employeeAttendance();
+		Employeewage employee2 = new Employeewage("microsoft", 34, 89, 37, 100);
+		employee2.employeeAttendance();
+		
+		EmpWageBuilder empBuilder = new EmpWageBuilder();
+		
+		empBuilder.showinfo(employee1);
+		empBuilder.addCompany(employee1);
+		
+		empBuilder.showinfo(employee2);
+		empBuilder.addCompany(employee2);
+	}	
+}
+
+
+
