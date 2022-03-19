d03f09a704543231a38e4b1766def16cc27248d2
nehachougule1998 <nchougule734@gmail.com>
2022-03-19 15:02:49 +0530

    UC10 added

---
+++ src/com/bridgelabz/CompanyEmpWage.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+public class CompanyEmpWage {
+
+	String name;
+	int wagePerHrs;
+	int workingDays;
+	int workingHoursMonthly;
+	int monthlyWage;	
+	
+	void setInfo(String name,int wagePerHrs,int workingDays, int workingHoursMonthly) {
+		 this.name = name;
+		 this.wagePerHrs = wagePerHrs;
+		 this.workingDays = workingDays;
+		 this.workingHoursMonthly = workingHoursMonthly;
+			   
+	}
+		   
+	void companyMonthlyWage(int monthlyWage) {
+		this.monthlyWage=monthlyWage;
+	}
+	}
+	
--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -1,16 +1,18 @@
 package com.bridgelabz;
+
 import java.util.ArrayList;
+
 public class EmpWageBuilder {
-
-private ArrayList<Employeewage> companyList = new ArrayList<Employeewage>();
+	
+ static ArrayList<CompanyEmpWage> companyList = new ArrayList<CompanyEmpWage>();
 	 
-	 void showinfo(Employeewage EmployeeWage) {
-		 System.out.println("Company name: "+EmployeeWage.name);
-		 System.out.println("Total Wage :"+EmployeeWage.monthlyWage);
+	 void showInfo(CompanyEmpWage company) {
+		 System.out.println("Company name: "+company.name);
+		 System.out.println("Total Wage :"+company.monthlyWage);
 	 }
 	 
-	 void addCompany(Employeewage EmployeeWage) {
-		 companyList.add(EmployeeWage);
-	 }
+	 void addCompany(CompanyEmpWage company) {
+		 companyList.add(company);
 
+	 }
 }
